a0291168b059e6d2a406bef209f2dffc8b4214be
Ross Bulat <ross@jkrbinvestments.com>
2020-07-16 16:47:27 +0700

    markdown

--- templates/welcome.js
+++ templates/welcome.js
@@ -3,10 +3,10 @@ module.exports = {
     subject: `A Warm Welcome!`,
     message: `Hello ${name !== '' ? ` ${name},` : `!`}
 
-Thank you for signing up to Chinese Grammar Review! 
+## Thank you for signing up!
 
-If you have any queries or concerns about the app, if something does not work, or if you have feedback for improvements, please feel free to contact us.
+If you have any queries or concerns about the app, if something does not work, or if you have [feedback](https://my-domain.com/contact) for improvements, please feel free to contact us.
 
-From Everyone at App Team`
+### From Everyone at App Team`
   })
 };
